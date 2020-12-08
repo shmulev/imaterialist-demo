aac87ff2c78e2f2d670b634ab7bc1065f3663cc6
Yaroslav Shmulev <yv.shmulev@jet.su>
2020-12-08 22:49:31 +0300

    add some util. functions

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-import torch.onnx
-import torch.onnx.symbolic_opset9 as onnx_symbolic
+# import torch.onnx
+# import torch.onnx.symbolic_opset9 as onnx_symbolic
 import segmentation_models_pytorch as smp
 from preprocessing import get_preprocessing
 from predict import predict_mask
@@ -65,47 +65,47 @@ def melt_masks(masks, class_values):
     return mask3D.astype('uint8')
 
 
-# function to save PyTorch model to ONNX
-def drop_onnx(model, model_path, input_tensor_example):
-    """
-    Save PyTorch model to ONNX format
-    :param model: pytorch model, instance of nn.Module
-    :param model_path: file path for saving
-    :param input_tensor_example: torch.Tensor input example
-    :param output_numpy: np.array output example
-    :return: None
-    """
-    print('Exporting model to ONNX...')
-    def upsample_nearest2d(g, input, output_size):
-        # Currently, TRT 5.1/6.0 ONNX Parser does not support all ONNX ops
-        # needed to support dynamic upsampling ONNX forumlation
-        # Here we hardcode scale=2 as a temporary workaround
-        scales = g.op("Constant", value_t=torch.tensor([1.,1.,2.,2.]))
-        return g.op("Upsample", input, scales, mode_s="nearest")
+# # function to save PyTorch model to ONNX
+# def drop_onnx(model, model_path, input_tensor_example):
+#     """
+#     Save PyTorch model to ONNX format
+#     :param model: pytorch model, instance of nn.Module
+#     :param model_path: file path for saving
+#     :param input_tensor_example: torch.Tensor input example
+#     :param output_numpy: np.array output example
+#     :return: None
+#     """
+#     print('Exporting model to ONNX...')
+#     def upsample_nearest2d(g, input, output_size):
+#         # Currently, TRT 5.1/6.0 ONNX Parser does not support all ONNX ops
+#         # needed to support dynamic upsampling ONNX forumlation
+#         # Here we hardcode scale=2 as a temporary workaround
+#         scales = g.op("Constant", value_t=torch.tensor([1.,1.,2.,2.]))
+#         return g.op("Upsample", input, scales, mode_s="nearest")
     
-    onnx_symbolic.upsample_nearest2d = upsample_nearest2d
+#     onnx_symbolic.upsample_nearest2d = upsample_nearest2d
     
-    if input_tensor_example is not None:
-        input_tensor_example = input_tensor_example.to('cpu')
+#     if input_tensor_example is not None:
+#         input_tensor_example = input_tensor_example.to('cpu')
        
-    torch.onnx.export(model.to('cpu'),  # model being run
-                      input_tensor_example,  # model input (or a tuple for multiple inputs)
-                      f"{model_path}",  # where to save the model (can be a file or file-like object)
-                      export_params=True,  # store the trained parameter weights inside the model file
-                      opset_version=11,  # the ONNX version to export the model to
-                      # do_constant_folding=True,  # whether to execute constant folding for optimization
-                      input_names=['input'],  # the model's input names
-                      output_names=['output'],  # the model's output names
-                      # dynamic_axes={'input': {0: 'batch_size'},  # variable lenght axes
-                      #               'output': {0: 'batch_size'}}
-                     )
-    print(f'Model is successfully saved to ONNX: {model_path}.')
+#     torch.onnx.export(model.to('cpu'),  # model being run
+#                       input_tensor_example,  # model input (or a tuple for multiple inputs)
+#                       f"{model_path}",  # where to save the model (can be a file or file-like object)
+#                       export_params=True,  # store the trained parameter weights inside the model file
+#                       opset_version=11,  # the ONNX version to export the model to
+#                       # do_constant_folding=True,  # whether to execute constant folding for optimization
+#                       input_names=['input'],  # the model's input names
+#                       output_names=['output'],  # the model's output names
+#                       # dynamic_axes={'input': {0: 'batch_size'},  # variable lenght axes
+#                       #               'output': {0: 'batch_size'}}
+#                      )
+#     print(f'Model is successfully saved to ONNX: {model_path}.')
     
     
-def load_model(model_path, Model, model_params, device):
-    model = Model(**model_params)
-    state = torch.load(model_path, map_location=device)
-    model.load_state_dict(state['state_dict'])
-    model = model.to(device)
-    model = model.eval()
-    return model
+# def load_model(model_path, Model, model_params, device):
+#     model = Model(**model_params)
+#     state = torch.load(model_path, map_location=device)
+#     model.load_state_dict(state['state_dict'])
+#     model = model.to(device)
+#     model = model.eval()
+#     return model
